1638dd9ac4b7524625bd67ee7552ab9d87fda83c
Jordan Carr <jcc117@pitt.edu>
2017-12-28 22:23:58 -0500

    begin report branch

--- KanjiApp/static/reports.js
+++ KanjiApp/static/reports.js
@@ -1,11 +1,24 @@
 //Event triggered by finishing quiz
 function send_report(score, total, dif)
 {
-	makeRequest('POST', '/report/', do_nothing, 201, [score, total, dif]);
+	var data = "score=" + score + "&total=" + total + "&dif=" + dif;
+	makeRequest('POST', '/report/', do_nothing, 201, data);
 }
 
 //Filler for above
 function do_nothing()
 {
 
+}
+
+//Ask for all reports for a user
+function get_report()
+{
+	makeRequest('GET', '/report/', handleReports, 200);
+}
+
+//Handle all of the reports from the request
+function handleReports()
+{
+	
 }
