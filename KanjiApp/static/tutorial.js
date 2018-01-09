01dad62b88b90ae70dabcef185b38357fd44411c
Jordan Carr <jcc117@pitt.edu>
2018-01-09 15:11:24 -0500

    typewriter effect very buggy

--- KanjiApp/static/tutorial.js
+++ KanjiApp/static/tutorial.js
@@ -5,17 +5,19 @@ var speed = 50;
 function t_setup()
 {
 	$('#tspan').click(start);
+	document.getElementById('d1').style.display = "none";
 }
 
 //Run the tutorial
 function start()
 {
+	document.getElementById('d1').style.display = "block";
 	clear();
+	document.getElementById("n1").addEventListener("click", explain_q2, true);
 	switch_to_q();
-	explain_q();
-	/*
-	switch_to_r();
-	explain_r();
+	explain_q1();
+	//switch_to_r();
+	/*explain_r();
 	switch_to_k();
 	explain_k();
 	switch_to_s();
@@ -27,22 +29,25 @@ function start()
 }
 
 //Explain how quizzes work
-function explain_q()
+function explain_q1()
 {
-	var text = "Welcome to KanjiMaster.com! We'll guide you some of the basics of this site."
+	var text = "Welcome to KanjiMaster.com! We'll guide you through some of the basics of this site."
 	typeWriter(text, i);
-	
-	//Clear the dialouge box for the next string
+}
+function explain_q2()
+{
 	clear();
-
-	text = "This is the quiz tab, where you can take quizzes. Each grade level tests you on a different set of kanji. By clicking on the correct button, you will be tested on 30 random kanji compounds."
+	var text =  "This is the quiz tab, where you can take quizzes. Each grade level tests you on a different set of kanji. By clicking on the correct button, you will be tested on 30 random kanji compounds."
 	typeWriter(text, i);
+	document.getElementById("n1").removeEventListener("click", explain_q2);
+	document.getElementById("n1").addEventListener("click", explain_r, true);
 }
 
 //Explain reports
 function explain_r()
 {
-
+	clear();
+	switch_to_r();
 }
 
 //Explain the kanji sheets
@@ -60,6 +65,7 @@ function explain_s()
 //Have typewriter effect for text
 function typeWriter(text, i)
 {
+	console.log(text.charAt(i) + i);
 	if(i < text.length)
 	{
 		document.getElementById("p1").innerHTML += text.charAt(i);
